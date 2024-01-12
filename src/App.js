05c4f0942062085dca4fcc4e23e74e6e1fb3d5f3
Bruno <Bruno>
2024-01-12 21:58:49 +0100

    testing context api with Enzyme

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import "./App.css";
 import Count from "./TESTINGWITHJEST/Count";
 import Todos from "./TESTINGWITHJEST/Todos";
 import { OrderedList } from "./TESTINGWITHENZYME/OrderedList";
+import { AuthProvider } from "./TESTINGWITHENZYME/provider/AuthContext";
+import { Login } from "./TESTINGWITHENZYME/Login";
 
 function App() {
   const animals = ["duck", "wales", "sheeps"];
   return (
     <div className="App">
-      <Count />
-      <Todos />
-      <OrderedList options={animals} />
+      <AuthProvider>
+        <Count />
+        <Todos />
+        <OrderedList options={animals} />
+
+        <Login />
+      </AuthProvider>
     </div>
   );
 }
