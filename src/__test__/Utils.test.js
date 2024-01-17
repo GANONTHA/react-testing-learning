48b4e4971e3f72b3f4dd3390a209afae7798894f
Bruno <Bruno>
2024-01-17 16:28:42 +0100

    adding a test in the utils file

--- src/__test__/Utils.test.js
+++ src/__test__/Utils.test.js
@@ -4,4 +4,7 @@ describe("Utils functions", () => {
   test("should format the name by adding @ at the beginning", () => {
     expect(formatName("@John")).toBe("@John");
   });
+  test("should format the name by adding @ at the beginning", () => {
+    expect(formatName("John")).toBe("@John");
+  });
 });
